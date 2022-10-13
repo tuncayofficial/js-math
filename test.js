e45a526870973a62aea507201614e1528602d6b5
tuncayofficial <qtuncay70@gmail.com>
2022-10-13 15:50:38 +0400

    test

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 const jsmath = require("./index")
 
-const cube = new jsmath.Cube(5, 6)
-console.log(cube)
+const { objectFriction } = jsmath.newton.functions
+
+let result = objectFriction(0.2, 3, 45)
+console.log(result)
