ec1dcb98a99723607520f99258e2e4b7cdefbdc3
tuncayofficial <qtuncay70@gmail.com>
2022-10-17 15:50:32 +0400

    creating atom model

--- src/physics/atom.js
+++ src/physics/atom.js
@@ -1,1 +1,10 @@
 // empty for some reasons, update will come in 5-6 months
+class Atom{
+    constructor(proton, neutron, electron){
+        this.proton = proton
+        this.electron = electron
+        this.neutron = neutron
+    }
+}
+
+module.exports = Atom
